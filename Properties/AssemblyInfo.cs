d478f5739de04693c4e937e3a2b5bb3dd24d31d4
arutofu <arutofu@yandex.ru>
2023-02-06 18:09:44 +1100

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Library_jan_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Library_jan_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0efdd751-38be-4e3b-83c4-9d7334937f06")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Library_jan;
+
+namespace Library_jan_Test
+{
+    //Использвуется структура AAA.
+    //---------------------
+    //Arrange    Подготовка
+    //Act        Действие
+    //Assert     Проверка
+    //---------------------
+
+    //Тестирование треугольника.
+    [TestClass]
+    public class TriangleTests
+    {
+        //Возвращает True если треугольник прямоугольный.
+        [TestMethod]
+        public void ifStraightReturnTrue()
+        {
+            var triangle = new Triangle("Треугольник", 3, 4, 5);
+
+            var result = triangle.isStraightTriangle();
+
+            Assert.IsTrue(result);
+        }
+
+        //Площадь треугольника со сторонами 3,4,5 = 6. Если ожидаемый и фактический результаты не совпали, создается исключение.
+        [TestMethod]
+        public void Square3_4_5Return6()
+        {
+            var triangle = new Triangle("Треугольник", 3, 4, 5);
+            double expected = 6;
+
+            var result = triangle.Square();
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+
+    //Тестирование Круга.
+    [TestClass]
+    public class CircleTest
+    {
+        //Площадь круга с радиусом 10 = 314.2. Если ожидаемый и фактический результаты не совпали, создается исключение.
+        [TestMethod]
+        public void Square10Return314_2()
+        {
+            var circle = new Circle("Круг", 10);
+            double expected = 314.2;
+
+            var result = circle.Square();
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
